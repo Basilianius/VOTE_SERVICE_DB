8e9788489acc76100637bcdbb0bd9e40099ebc76
Basilianius <basilianius@gmail.com>
2015-04-12 16:41:52 +0300

    добавил тестовый скрипт заполнения данными

---
+++ test_update_db.sql
@@ -0,0 +1,3 @@
+insert into teams_dir values (1, 'TestTeam', 1);
+insert into employees_dir values (1, 'FirstName', 'LastName', 1, TRUE);
+insert into intervals_dir values (1, '2015-04-01', '2015-04-30', '2015-04', TRUE);
